7b42026ce6b65f7cbe3dd27a3b2504ed72bcbb00
quentinrouger <quentin_rouger@hotmail.fr>
2023-02-20 02:58:41 -0800

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Write a program that prints the alphabet in lowercase
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
