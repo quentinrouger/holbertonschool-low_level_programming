d1fbb1e1e56cc8d16dd589e2229d0be85adb6741
quentinrouger <quentin_rouger@hotmail.fr>
2023-02-21 00:01:28 -0800

    3-islower

--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -4,3 +4,5 @@
 
 2. 10 x alphabet
 
+3. islower
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,3 +12,8 @@ void print_alphabet(void);
 *Write a function that prints 10 times the alphabet, in lowercase
 */
 void print_alphabet_x10(void);
+
+/**
+*Write a function that checks for lowercase character.
+*/
+int _islower(int c);
