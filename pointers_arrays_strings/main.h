7b1ea154a363afa145d1561d73754166b4ea800e
quentinrouger <quentin_rouger@hotmail.fr>
2023-02-27 06:34:47 -0800

    6-puts2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -29,3 +29,10 @@ void print_rev(char *s);
 *Write a function that reverses a string.
 */
 void rev_string(char *s);
+
+/**
+*Write a function that prints every other character of a string,
+*starting with the first character, followed by a new line.
+*/
+void puts2(char *str);
+
