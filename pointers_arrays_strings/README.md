b3245bc475c70fedb61b18c4e90a4979db7f890a
quentinrouger <quentin_rouger@hotmail.fr>
2023-03-01 02:52:05 -0800

    1-strncat

--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -27,3 +27,5 @@ More pointers, arrays and strings
 
 0. strcat
 
+1. strncat
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,3 +30,5 @@ int _atoi(char *s);
 */
 char *_strcat(char *dest, char *src);
 
+char *_strncat(char *dest, char *src, int n);
+
