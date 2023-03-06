4e98521cbc52914aac22aa60a6d3783de231c791
quentinrouger <quentin_rouger@hotmail.fr>
2023-03-06 07:46:49 -0800

    2-strlen_recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_strlen_recursion - Write a function that returns
+*the length of a string.
+*@s: pointer
+*
+*Return: strlen
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return (0);
+}
+else
+{
+return (1 + (_strlen_recursion(s + 1)));
+}
+}
--- recursion/README.md
+++ recursion/README.md
@@ -2,3 +2,5 @@
 
 1. Why is it so important to dream? Because, in my dreams we are together
 
+2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange
+
