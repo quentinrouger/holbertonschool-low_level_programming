3cb1f3d6940eeb0a34ad9e9b509b699231699b27
quentinrouger <quentin_rouger@hotmail.fr>
2023-03-07 07:10:40 -0800

    2-args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - prints all arguments
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
