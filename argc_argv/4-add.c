91f5a2f7b65f28cb188443a6bd7bc342962fc2cb
quentinrouger <quentin_rouger@hotmail.fr>
2023-03-07 07:58:22 -0800

    4-add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - program that adds positive numbers.
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+int i, j, sum;
+sum = 0;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return (0);
+}
--- argc_argv/README.md
+++ argc_argv/README.md
@@ -6,3 +6,4 @@
 
 3. Neither irony nor sarcasm is argument
 
+4. To infinity and beyond
